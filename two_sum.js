b11c51c886410345d72d68c7ce01a9ecfd656793
ianellison <ianellison87@yahoo.com>
2019-10-16 12:00:54 -0700

    twoSum

---
+++ two_sum.js
@@ -0,0 +1,30 @@
+//  O(n^2) time | O(1) space
+function twoNumberSum(array, targetSum) {
+
+}
+
+// O(n) time | O(n) space
+function twoNumberSum(array, targetSum) {
+  const nums = {}
+
+  for (let i = 0; i < array.length; i++) {
+    const potentialNum = targetSum - array[i];
+    
+    if (potentialNum in nums) {
+      return [potentialNum , array[i]].sort((a,b) => a - b);
+    } else {
+      nums[array[i]] = true;
+    }
+  }
+  return [];
+}
+
+function twoNumberSum(array, targetSum) {
+
+}
+
+// array = [1,5,11,-1,6,3]
+// target sum = 10
+// expected output = [11,-1]
+
+
